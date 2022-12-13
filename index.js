c393dbd812717bd0df6f96534d87da6a703a871b
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-13 17:09:24 +0300

    Corrected Assignment

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// Code your solutions in this file
-function writeCards(spy){
-    for(let i = 0; i <=spy.length+1; i++){
-        const arr =[]
-        // arr.push(`Thank you, ${spy[0]},${spy[1]},${spy[2]}, for the wonderful surprise gift!`)
-        arr.push(`Thank you, ${spy}, for the wonderful surprise gift!`)
-        return arr
+// // Code your solutions in this file
+function writeCards(spy,event){
+    const arr =[]
+    for(let i = 0; i <spy.length; i++){
+        arr.push(`Thank you, ${spy[i]}, for the wonderful ${event} gift!`)
     }
-    console.log(arr)
+    return arr
 }
-// function countDown(spy){
-//     for(let j=0;j <= spy;j++)
-//         console.log(j)
-//     }
 function countDown(){
     let num = 0
     while(num <11){
         console.log(num++)
     }
 }
-writeCards(["Mark","Bob","Sandra"])
+
